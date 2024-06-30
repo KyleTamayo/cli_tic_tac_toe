fa2e0bb5712278e42716fca4ad4fc046e8d16c6d
kyletamayo <kyle_tamayo@startmail.com>
2024-06-29 21:59:33 -0400

    messing around on other computer

---
+++ cmake-build-debug/CMakeFiles/cli_tic_tac_toe.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cli_tic_tac_toe.dir/main.c.o"
+  "CMakeFiles/cli_tic_tac_toe.dir/main.c.o.d"
+  "cli_tic_tac_toe"
+  "cli_tic_tac_toe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cli_tic_tac_toe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/cli_tic_tac_toe.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cli_tic_tac_toe.
+# This may be replaced when dependencies are built.
